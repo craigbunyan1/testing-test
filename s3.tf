ce9c96a5c1fca9b9c34e18606f86e364bd7992e4
Craig Bunyan <craig.bunyan1@sainsburys.co.uk>
2019-11-19 17:07:04 +0000

    disable required input for tf apply, add details in tf for existing and additional bucket

--- s3.tf
+++ s3.tf
@@ -3,11 +3,21 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "b" {
+  bucket = "testing-test-tf-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My second bucket - ci test"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket" "b" {
   bucket = "testing-test-tf-bucket-ci"
   acl    = "private"
 
   tags = {
-    Name        = "My third bucket"
+    Name        = "My third bucket - ci test"
     Environment = "Dev"
   }
 }
